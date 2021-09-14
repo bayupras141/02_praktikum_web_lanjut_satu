c7274ab1a0cec7111310b6770cce933a8be4ad95
Bayu Prasetyo <aebayu789@gmail.com>
2021-09-14 11:43:45 +0700

    Praktikum3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,9 +21,9 @@ use App\Http\Controllers\PageController;
 //     return view('welcome');
 // });
 
-// Route::Get('/', function () {
-//     echo "Selamat Datang";
-// });
+Route::Get('/', function ($id) {
+    echo "Selamat Datang";
+});
 
 Route::Get('/about', function () {
     echo "NIM : 2031710161";
@@ -31,4 +35,40 @@ Route::Get('/articles/{id}', function ($id) {
     return $id;
 });
 
-Route::Get('/', [PageController::class, 'index']);
+// Route::Get('/', [HomeController::class, 'index']);
+
+Route::group(["prefix"=>"/program"], function(){
+    Route::get("/karir", function(){
+        echo "Karir";
+    });
+    Route::get("/magang", function(){
+        echo "Magang";
+    });
+    Route::get("/kunjungan-industri", function(){
+        echo "Kunjungan industri";
+    });
+});
+
+Route::get("/news/{id}", function($id){
+    return "news ".$id;
+});
+
+Route::group(["prefix"=>"/category"], function(){
+    Route::get("/marbbel-edu-games", function(){
+        echo "Marbel edu games";
+    });
+    Route::get("/marbbel-and-friends-kid-games", function(){
+        echo "Marbel kid games";
+    });
+    Route::get("/riri-story-books", function(){
+        echo "Riri story books";
+    });
+    Route::get("/kolak-kids-songs", function(){
+        echo "Kolak story";
+    });
+});
+
+Route::Get('/about-us', [AboutController::class, 'about']);
+Route::resource('/contact-us', ContactController::class);
+
+    
